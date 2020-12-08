f4b50c6763a9252068b32b67b035c5182b1e37ed
AjaySharma91020 <ajsharma7231994697@gmail.com>
2020-12-08 18:20:41 +0530

    aligned icons to center

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -3,19 +3,20 @@ import React from "react";
 function Square(props) {
   const { name } = props;
   return (
-    <div className="square border border-primary" onClick={() => props.onClickChange(props.index)}>
-      <div className="py-1 text-center">
-        {name &&
-          (name === "O" ? (
-            <span className="text-secondary">
-              <i className="far fa-circle"></i>
-            </span>
-          ) : (
-            <span className="text-danger">
-            <i className="fas fa-times"></i>
-            </span>
-          ))}
-      </div>
+    <div
+      className="square border border-primary"
+      onClick={() => props.onClickChange(props.index)}
+    >
+      {name &&
+        (name === "O" ? (
+          <span className="icon text-secondary">
+            <i className="far fa-circle"></i>
+          </span>
+        ) : (
+          <span className="icon text-danger">
+            <i className=" fas fa-times"></i>
+          </span>
+        ))}
     </div>
   );
 }
